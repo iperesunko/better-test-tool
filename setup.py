38046702f0d10b2f5fef4cd006f95d245123effd
Ihor Peresunko <ihor.peresunko@gmail.com>
2018-10-21 22:42:51 +0300

    Increased a version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="bds_test_tool",
-    version="0.0.1",
+    version="0.1.0",
     author="Ihor Peresunko",
     author_email="ihor.peresunko@genesys.com",
     description="A tool for easy testing the Billing Data Server",
     url="https://gitlab.com/curious22/tools",
-    packages=find_packages(exclude=['*.tests.*', 'file-fixtures']),
+    packages=find_packages(exclude=['tests', 'file-fixtures']),
     include_package_data=True,
     entry_points={
         'console_scripts': ['btt=bds_test_tool.main:main'],
