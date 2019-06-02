799ea30d9b434ac507aca95601fb6929556f1cc0
Ihor Peresunko <ihor.peresunko@gmail.com>
2019-06-02 20:30:37 +0300

    Utils tests have fixed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from better_test_tool import utils
@@ -5,11 +7,11 @@ from better_test_tool import utils
 
 def test_get_folder_time():
     result = utils.get_folder_time('file-fixtures')
-    assert result == 1544035211.4587939
+    assert result == os.stat('file-fixtures').st_mtime
 
 
 def test_is_folder_modified():
-    result = utils.is_folder_modified(1544035211.4587939, 'file-fixtures')
+    result = utils.is_folder_modified(os.stat('file-fixtures').st_mtime, 'file-fixtures')
     assert result is False
 
     result = utils.is_folder_modified(1544041029.8485782, 'file-fixtures')
@@ -42,7 +44,6 @@ def test_filter_folders():
     assert ['tests'] == utils.filter_folders(folders, 'te')
 
 
-@pytest.fixture
 def get_search_word():
     return 'fil'
 
